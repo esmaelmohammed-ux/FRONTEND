c42a09711f986f018c5ee242bdd853d21fa44fec
Esmael <esmael6360@gmail.com>
2026-02-19 10:40:52 +0300

    Update ExploreMenu.jsx

--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./exploremenu.css";
+import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = ({ category, setCategory }) => {
